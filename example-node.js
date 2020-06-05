d0f5952fcfee385c75e8db983349f3f35d142320
Shubhamjit Singh <200395159@student.georgianc.on.ca>
2020-06-04 21:11:41 -0400

    lab2

--- example-node.js
+++ example-node.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 const https = require('https');
 
-app.get('/', (request, response) => (
-    https.get('https://insult.mattbas.org/api/insult' , resp => {
-        resp.on('data', chunk => {
-            response.send(`
-                <http>
-                <head><title>Oh Snap</title></head>
+app.get('/', (request, response) => {
+  https.get('https://insult.mattbas.org/api/insult', resp => {
+    resp.on('data', chunk => {
+      response.send(`
+        <http>
+          <head><title>Oh Snap</title></head>
 
-                <body>
-                    <h1>Oh Snap</h1>
-                    <p style="font-sizze: 24px; color: red;">
-                    ${chunk.toString()}!
-                    </p>
-                </body>
-            </http>
-            `);
-        });
-    })
-));
+          <body>
+            <h1>OH SNAP!</h1>
+            <p style="font-size: 24px; color: red;">
+              ${chunk.toString()}!
+            </p>
+          </body>
+        </http>
+      `);
+    });
+  });
+});
 
 app.listen(process.env.PORT || 3000);
